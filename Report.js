742b7f850f0d314e5a402766a10b30bfea4a437c
JuanJoZP <juanzpjose136@gmail.com>
2024-05-02 13:03:34 -0500

    closes #21

---
+++ Report.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react"
+import { StyleSheet, View } from "react-native"
+import { useTheme, Text, TextInput, Button } from "react-native-paper"
+import Constants from "expo-constants"
+
+import InputAutocomplete from "./InputAutocomplete"
+
+const incidentsInfo = {
+  "01": {
+    title: "robo",
+    description: "Si has presenciado un robo bla bla bla",
+  },
+  // AQUI VAN LOS OTROS INCIDENTES
+}
+
+export default function Report({ route, navigation }) {
+  const { incidentId } = route.params
+  const { title, description } = incidentsInfo[incidentId]
+  const [coment, setComent] = useState("")
+  const [address, setAddres] = useState("")
+  const theme = useTheme()
+
+  return (
+    <View style={styles.container}>
+      <Text variant="headlineMedium" style={styles.title}>
+        Reportar {title}
+      </Text>
+      <Text variant="bodyLarge" style={styles.description}>
+        {description}
+      </Text>
+
+      <Text variant="titleMedium">Lugar del incidente</Text>
+      <TextInput
+        label="Dirección"
+        mode="outlined"
+        value={address}
+        style={styles.input}
+        onChangeText={address => {
+          setAddres(address)
+        }}
+      />
+
+      <Text variant="titleMedium">Comentario</Text>
+      <TextInput
+        multiline
+        label="Comentario"
+        mode="outlined"
+        numberOfLines={3}
+        value={coment}
+        style={styles.input}
+        onChangeText={coment => {
+          setComent(coment)
+        }}
+      />
+
+      <View style={styles.button}>
+        <Button mode="contained" onPress={() => console.log("Pressed")}>
+          Reportar
+        </Button>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 20,
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  description: {
+    marginBottom: 20,
+  },
+})
